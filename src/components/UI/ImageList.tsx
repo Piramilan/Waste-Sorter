99b7aac34d3cb3daa893e5bb5b0c3a8fa39e56de
Piramilan <piramilanmessi@gmail.com>
2023-03-29 20:39:20 +0530

    v1.13.0

--- src/components/UI/ImageList.tsx
+++ src/components/UI/ImageList.tsx
@@ -33,7 +33,7 @@ const ImageList: React.FC<ImageListProps> = ({ images }) => {
   }, [images]);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-      {isMobile && (
+      {predictionImages && predictionImages.length >= 2 && isMobile && (
         <div className="flex justify-end items-center max-w-[85vw]">
           <span className="text-slate-300 mr-4">Activate Carousel View</span>
           <ToggleButton value={carouselActivate} onClick={onCarouselActivate} />
