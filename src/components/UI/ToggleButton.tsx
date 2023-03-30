aec67a33f56dbd1efa9a2b70ae9f418c57523784
Piramilan <piramilanmessi@gmail.com>
2023-03-31 00:27:38 +0530

    v1.18.0

--- src/components/UI/ToggleButton.tsx
+++ src/components/UI/ToggleButton.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, ChangeEventHandler } from "react";
+import React from "react";
 
 interface Props {
   value: boolean;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,12 +4,10 @@ import * as React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Loading from "@/components/UI/Loading";
-import { useModel } from "@/lib/ModelContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [isLoading, setIsLoading] = React.useState(true);
   const router = useRouter();
-  const { model } = useModel();
 
   React.useEffect(() => {
     setTimeout(() => {
